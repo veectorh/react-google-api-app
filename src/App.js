0f610c4df8fdff3ddc1095eee51a99faeb89c0f0
veectorh <blackjetv5@gmail.com>
2020-05-03 21:57:08 +0100

    generic style and index routes for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import { Allprovider } from './context/AllContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Previews from './components/Previews';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Allprovider>
+      <div className="App">
+        <div className='container'>
+        <Router>
+          <Route path='/' exact component={Login} ></Route>
+          <Route path='/home'  component={Home} ></Route>
+          <Route path='/books/:bookid' exact component={Previews}></Route>
+        </Router>
+        </div>
+      </div>
+    </Allprovider>
   );
 }
 
